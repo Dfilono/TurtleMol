bc3b98b98bc1afa3c341f2d79d06d164bf2cb74e
Dominick Filonowich <filonowichd74@gmail.com>
2023-11-20 00:14:30 -0500

    versioneer debugging

--- TurtleMol/_version.py
+++ TurtleMol/_version.py
@@ -50,8 +50,8 @@ def get_config() -> VersioneerConfig:
     # _version.py
     cfg = VersioneerConfig()
     cfg.VCS = "git"
-    cfg.style = "pep440-old"
-    cfg.tag_prefix = "v"
+    cfg.style = "pep440"
+    cfg.tag_prefix = ""
     cfg.parentdir_prefix = "None"
     cfg.versionfile_source = "TurtleMol/_version.py"
     cfg.verbose = False
