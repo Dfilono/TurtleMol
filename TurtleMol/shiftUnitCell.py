346771805362a671678b668f43fe3ed716fcea3b
Dominick Filonowich <filonowichd74@gmail.com>
2024-04-01 15:03:54 -0400

    Changed new unit cell calculation

--- TurtleMol/shiftUnitCell.py
+++ TurtleMol/shiftUnitCell.py
@@ -14,7 +14,7 @@ def unitCellBox(shape, dims, cellDims, og, radii):
 
     # Calculate how many times to duplicate the unit cell in a given dimension
     dupeCount = [int(dims[i] / cellDims[i]) for i in range(3)]
-    cellParams = f'CRYST1    {dupeCount[0]*cellDims[0] - totalRadius: .3f}    {dupeCount[1]*cellDims[1] - totalRadius: .3f}    {dupeCount[2]*cellDims[2] - totalRadius: .3f}  90.00  90.00  90.00 P1          1'
+    cellParams = f'CRYST1    {dupeCount[0]*cellDims[0]: .3f}    {dupeCount[1]*cellDims[1]: .3f}    {dupeCount[2]*cellDims[2]: .3f}  90.00  90.00  90.00 P1          1'
 
     filled = []
 
@@ -104,7 +104,7 @@ def unitCellMesh(shape, cellDims, og, radius):
     boxDim = maxBound - minBound
     dupeCount = [int(boxDim[i] / cellDims[i]) for i in range(3)]
     
-    cellParams = f'CRYST1    {dupeCount[0]*cellDims[0] - totalRadius: .3f}    {dupeCount[1]*cellDims[1] - totalRadius: .3f}    {dupeCount[2]*cellDims[2]: .3f}  90.00  90.00  90.00 P1          1'
+    cellParams = f'CRYST1    {dupeCount[0]*cellDims[0]: .3f}    {dupeCount[1]*cellDims[1]: .3f}    {dupeCount[2]*cellDims[2]: .3f}  90.00  90.00  90.00 P1          1'
 
     filled = []
 
