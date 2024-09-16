Fixed bugs with rotation

TurtleMol/shiftDensity.py
@@ -45,7 +45,7 @@ def placeMols(shape, og, density, tol, shapeType, radii, randOrient,rotAngles):
         if randOrient and len(newMol) == len(og):
                     newMol = Reorient(newMol, randRotate=True)
 
-        if rotAngles != [0, 0, 0] and len(newMol) == len(og):
+        if np.array(rotAngles).all() != np.array([0, 0, 0]).all() and len(newMol) == len(og):
             newMol = Reorient(newMol, angles=rotAngles)
 
         if len(og) == 1:

TurtleMol/shiftMesh.py
@@ -107,7 +107,7 @@ def moleculesFillMesh(mesh, og, tol, radii, numMol, baseStruc,
                         
                     if randOrient and len(newMol) == len(og):
                         newMol = Reorient(newMol, randRotate=True)
-                    if rotAngles != [0, 0, 0] and len(newMol) == len(og):
+                    if np.array(rotAngles).all() != np.array([0, 0, 0]).all() and len(newMol) == len(og):
                         newMol = Reorient(newMol, angles=rotAngles)
                     if alignNormal:
                         newMol = alignToNormal(mesh, newMol)
@@ -198,7 +198,7 @@ def moleculesRandMesh(mesh, og, tol, radii, numMol, baseStruc,
         if randOrient and len(newMol) == len(og):
             newMol = Reorient(newMol, randRotate=True)
 
-        if rotAngles != [0, 0, 0] and len(newMol) == len(og):
+        if np.array(rotAngles).all() != np.array([0, 0, 0]).all() and len(newMol) == len(og):
             newMol = Reorient(newMol, angles=rotAngles)
 
         if (kdTree is None or not isOverlapMoleculeKDTree(newMol, kdTree, indexToAtom, radii, tol)):

TurtleMol/shiftSphere.py
@@ -138,7 +138,7 @@ def moleculeFillSphere(numShifts, sphere, og, radii, tol,
                 if randOrient and len(newMol) == len(og):
                     newMol = Reorient(newMol, randRotate=True)
 
-                if rotAngles != [0, 0, 0] and len(newMol) == len(og):
+                if np.array(rotAngles).all() != np.array([0, 0, 0]).all() and len(newMol) == len(og):
                     newMol = Reorient(newMol, angles=rotAngles)
 
                 if (kdTree is None or not isOverlapMoleculeKDTree(newMol, kdTree, indexToAtom, radii, tol)) and \
@@ -199,7 +199,7 @@ def moleculeRandSphere(numMol, maxAttempts, og, sphere, radii, tol,
         if randOrient and len(newMol) == len(og):
                     newMol = Reorient(newMol, randRotate=True)
 
-        if rotAngles != [0, 0, 0] and len(newMol) == len(og):
+        if np.array(rotAngles).all() != np.array([0, 0, 0]).all() and len(newMol) == len(og):
             newMol = Reorient(newMol, angles=rotAngles)
 
         if (kdTree is None or not isOverlapMoleculeKDTree(newMol, kdTree, indexToAtom, radii, tol)):

TurtleMol/shiftUnitCell.py
@@ -6,14 +6,14 @@ from .shiftBox import inBox
 from .makeStruc import calcLatticeVectors, rotateUnitCell
 
 # Box
-def unitCellBox(shape, dims, cellDims, cellAngles, og, radii, rotAngles=[0, 0, 0]):
+def unitCellBox(shape, dims, cellDims, cellAngles, og, radii, rotAngles=np.array([0, 0, 0]).all()):
     '''Duplicates unit cells to fill a given box'''
 
     # Find the atom types in the tile
     atomNames = {atom[0] for atom in og}
     totalRadius = sum(radii[str.capitalize(name)] for name in atomNames)
 
-    if rotAngles != [0, 0, 0]:
+    if np.array(rotAngles).all() != np.array([0, 0, 0]).all():
         latticeVec = calcLatticeVectors(cellDims[0], cellDims[1], cellDims[2], cellAngles[0], cellAngles[1], cellAngles[2])
         og, cellInfo = rotateUnitCell(latticeVec, og, rotAngles)
         cellDims = [cellInfo['a'], cellInfo['b'], cellInfo['c']]
@@ -74,13 +74,13 @@ def unitCellBox(shape, dims, cellDims, cellAngles, og, radii, rotAngles=[0, 0, 0
                 filled.append(currentCell)
     return filled, "molecule", cellParams
 
-def unitCellSphere(shape, cellDims, cellAngles, og, radii, rotAngles=[0,0,0]):
+def unitCellSphere(shape, cellDims, cellAngles, og, radii, rotAngles=np.array([0, 0, 0]).all()):
     '''Duplicates unit cells to fill a given sphere'''
 
     # Box dimensions that completely contain the sphere
     boxDim = [2 * shape.radius] * 3
 
-    if rotAngles != [0, 0, 0]:
+    if np.array(rotAngles).all() != np.array([0, 0, 0]).all():
         latticeVec = calcLatticeVectors(cellDims[0], cellDims[1], cellDims[2], cellAngles[0], cellAngles[1], cellAngles[2])
         og, cellInfo = rotateUnitCell(latticeVec, og, rotAngles)
         cellDims = [cellInfo['a'], cellInfo['b'], cellInfo['c']]
@@ -128,7 +128,7 @@ def unitCellSphere(shape, cellDims, cellAngles, og, radii, rotAngles=[0,0,0]):
                 filled.append(currentCell)
     return filled, "molecule", cellParams
 
-def unitCellMesh(shape, cellDims, cellAngles, og, radius, rotAngles=[0,0,0]):
+def unitCellMesh(shape, cellDims, cellAngles, og, radius, rotAngles=np.array([0, 0, 0]).all()):
     '''Duplicates unit cells to fill a given mesh'''
 
     # Find the atom types in the tile
@@ -139,7 +139,7 @@ def unitCellMesh(shape, cellDims, cellAngles, og, radius, rotAngles=[0,0,0]):
     maxBound, minBound = shape.bounds[1], shape.bounds[0]
     boxDim = maxBound - minBound
 
-    if rotAngles != [0, 0, 0]:
+    if np.array(rotAngles).all() != np.array([0, 0, 0]).all():
         latticeVec = calcLatticeVectors(cellDims[0], cellDims[1], cellDims[2], cellAngles[0], cellAngles[1], cellAngles[2])
         og, cellInfo = rotateUnitCell(latticeVec, og, rotAngles)
         cellDims = [cellInfo['a'], cellInfo['b'], cellInfo['c']]

TurtleMol/surfaceNormal.py
@@ -13,7 +13,7 @@ def alignToNormal(mesh, atoms):
     normals = mesh.face_normals
     zAxis = np.array([0, 0, 1])
 
-    if zAxis == normals[normalIdx[0][0]]:
+    if zAxis.all() == normals[normalIdx[0][0]].all():
         return atoms
 
     rotationMatrix = alignVectors([zAxis], [normals[normalIdx[0][0]]])
@@ -44,7 +44,7 @@ def placeOnSurfaceNormal(mesh, atoms):
     normals = mesh.face_normals
     zAxis = np.array([0, 0, 1])
 
-    if zAxis == normals[normalIdx[0][0]]:
+    if zAxis.all() == normals[normalIdx[0][0]].all():
         return atoms
 
     rotationMatrix = alignVectors([zAxis], [normals[normalIdx[0][0]]])